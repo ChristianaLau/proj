fe58bb97552b694d582093ccc8435b9e710b58d7
ChristianaLau <CHRISTIANA.LAU25@myhunter.cuny.edu>
2024-06-18 19:52:52 -0400

    minor fixes to page

---
+++ nutrixa-auth/app/quiz/page.jsx
@@ -0,0 +1,11 @@
+const Quiz = () => {
+    return (
+      <>
+        <h1 className='text-2xl font-bold mb-5'>quiz</h1>
+        <p className='mb-5'>Welcome to the quiz!</p>
+      </>
+    );
+  };
+  
+  export default Quiz;
+  
