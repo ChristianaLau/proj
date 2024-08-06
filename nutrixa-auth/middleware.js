700ad4a2500f528c55525906fd9b498029cbd222
chrizztinna <116527657+chrizztinna@users.noreply.github.com>
2024-08-06 02:48:24 -0400

    Update middleware.js

--- nutrixa-auth/middleware.js
+++ nutrixa-auth/middleware.js
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/', '/profile', '/register', '/api/webhooks/clerk'],
+  publicRoutes: ['/', '/profile', '/register', '/api/webhooks/clerk', "/more-info", "/more-info2", "/more-info3", "/more-info4"],
 });
 
 export const config = {
