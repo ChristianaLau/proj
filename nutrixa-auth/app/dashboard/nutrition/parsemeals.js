b506c2e7073f785e991a2f6387c14e0eb4656d21
me <me>
2024-08-11 16:33:25 -0400

    parsing update

--- nutrixa-auth/app/dashboard/nutrition/parsemeals.js
+++ nutrixa-auth/app/dashboard/nutrition/parsemeals.js
@@ -1,14 +1,33 @@
 export const extractIngredients = (meal) => {
+    //exclude types of meals and articles
     const excludedWords = ['with', 'and', 'day', 'breakfast', 'lunch', 'dinner', 'a', '**'];
     const mealTimes = ['breakfast', 'lunch', 'dinner', 'snack'];
-    const mealWithoutTime = mealTimes.reduce((result, time) => result.replace(new RegExp(`^${time}:\\s*`, 'i'), ''), meal);
-    const words = mealWithoutTime.split(/\s+/);
-    const filteredWords = words.filter(word => !excludedWords.includes(word.toLowerCase()));
-    return filteredWords;
+
+    //get rid of time in output
+    const mealWithoutTime = mealTimes.reduce((result, time) => 
+      result.replace(new RegExp(`^${time}:\\s*`, 'i'), ''), meal
+    );
+
+    //split by comma
+    const ingPhrase = mealWithoutTime.split(/,\s*/);
+
+    //split more
+    const ingredients = ingPhrase.flatMap(ingredient =>
+      ingredient.split(/\s+and\s+|\s+with\s+/).map(word => word.trim())
+    );
+
+    //filer
+    const filteredIngredients = ingredients.filter(ingredient => {
+      const words = ingredient.split(/\s+/);
+      return words.some(word => !excludedWords.includes(word.toLowerCase()));
+    });
+    
+    return filteredIngredients;
+
   };
   
- export const extractMealSentence = (meal, type) => {
-    const mealphrase = new RegExp(`${type}:([^.]*)`, 'i');
-    const match = meal.match(mealphrase);
+ export const extractMealSentence = (day, meal) => {
+    const regex = new RegExp(`${meal}: ([^*]+)`);
+    const match = day.match(regex);
     return match ? match[1].trim() : null;
   };
