0c2e696c4b04490fcf0107754213aa23bf769a9d
me <me>
2024-08-13 16:45:38 -0400

    footer updates 1

---
+++ nutrixa-auth/app/footer/contact/page.js
@@ -0,0 +1,12 @@
+import {ContactForm} from './form';
+
+export default function Contact()
+{
+   return(
+    <main>
+        <h1>Contact
+            <ContactForm />
+        </h1>
+    </main>
+)
+}
