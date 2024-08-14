e9403f4bdea5c5feb771e75bd861ceaa3afa73b5
me <me>
2024-08-14 01:41:03 -0400

    weight tracker

--- nutrixa-auth/app/api/weight/route.js
+++ nutrixa-auth/app/api/weight/route.js
@@ -1,25 +1,51 @@
 import connect from '@/lib/db';
-import WeightUpdate from '../../trackers/weight/weighttrack'
+import { currentUser } from '@clerk/nextjs/server';
+import mongoose from "mongoose";
 import { NextResponse } from 'next/server';
 
 export async function POST(req) {
-    try {
-      const { weight } = await req.json();
-  
-      await connect();
-      console.log("Connected to DB");
-  
-      const newWeight = await WeightUpdate.create({ weight });
-      console.log("New Weight Entry: ", newWeight);
-  
-      return NextResponse.json({ success: true, data: newWeight }, { status: 201 });
-    } catch (error) {
-      console.error('Error handling POST request:', error);
-      return NextResponse.json({ success: false, error: error.message }, { status: 400 });
+  try {
+    const user = await currentUser();
+    if (!user) {
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
     }
+
+    const { weight } = await req.json();
+    const email = user.emailAddresses[0].emailAddress;
+
+    await connect();
+    console.log("Connected to DB");
+
+    const db = mongoose.connection.db;
+    const collection = db.collection("Nutrixa_Users");
+
+    const userDoc = await collection.findOne({ email: email });
+    console.log("User Document: ", userDoc);
+
+    if (userDoc) {
+      const updatedUserDoc = await collection.findOneAndUpdate(
+        { email: email },
+        {
+          $push: {
+            "person.weight_update": {
+              date: new Date(),
+              weight: weight,
+            },
+          },
+        },
+        { returnDocument: 'after' }
+      );
+
+      return NextResponse.json({ success: true, data: updatedUserDoc.value }, { status: 200 });
+    } else {
+      return NextResponse.json({ message: 'User not found' }, { status: 404 });
+    }
+  } catch (error) {
+    console.error('Error handling POST request:', error);
+    return NextResponse.json({ success: false, error: error.message }, { status: 500 });
   }
-  
-  export function OPTIONS() {
-    return NextResponse.json({}, { status: 200, headers: { Allow: 'POST' } });
-  }
-  
+}
+
+export function OPTIONS() {
+  return NextResponse.json({}, { status: 200, headers: { Allow: 'POST' } });
+}
