8e94abd5f6384f4c5fb288422b67223234e70e67
chrizztinna <116527657+chrizztinna@users.noreply.github.com>
2024-08-15 15:16:09 -0400

    Update more info1 page.jsx

--- nutrixa-auth/app/more-info/page.jsx
+++ nutrixa-auth/app/more-info/page.jsx
@@ -17,7 +17,7 @@ const MoreInfoPage = () => {
       and available resources.
       </h2> 
 
-      <div className="w-[510px] h-[610px] bg-[#ebfccb] rounded-lg m-5 mt-[250px] ml-[100px] flex flex-col justify-center items-center text-center p-5">
+      <div className="w-[510px] h-[610px] bg-[#ebfccb] rounded-lg m-5 mt-[250px] ml-[150px] flex flex-col justify-center items-center text-center p-5">
         <div className="text-center p-4">
           <span className="uppercase font-bold text-lg">SQUATS</span>
         </div>
@@ -31,7 +31,7 @@ const MoreInfoPage = () => {
         </div>
       </div>
 
-      <div className="w-[510px] h-[610px] bg-[#ebfccb] rounded-lg m-5 mt-[-627px] ml-[700px] flex flex-col justify-center items-center text-center p-5">
+      <div className="w-[510px] h-[610px] bg-[#ebfccb] rounded-lg m-5 mt-[-627px] ml-[750px] flex flex-col justify-center items-center text-center p-5">
         <div className="text-center p-5 mb-7">
           <span className="uppercase font-bold text-lg">BICEP CURLS</span>
         </div>
@@ -45,7 +45,7 @@ const MoreInfoPage = () => {
       </div>
 
 
-      <div className="w-[510px] h-[610px] bg-[#ebfccb] rounded-lg m-5 mt-[70px] ml-[100px] flex flex-col justify-center items-center text-center p-5">
+      <div className="w-[510px] h-[610px] bg-[#ebfccb] rounded-lg m-5 mt-[70px] ml-[150px] flex flex-col justify-center items-center text-center p-5">
         <div className="text-center p-5 mb-10 -mt-20">
           <span className="uppercase font-bold text-lg">PLANKS</span>
         </div>
@@ -61,7 +61,7 @@ const MoreInfoPage = () => {
 
 
 
-      <div className="w-[510px] h-[610px] bg-[#ebfccb] rounded-lg m-5 mt-[-630px] ml-[700px] flex flex-col justify-center items-center text-center p-5">
+      <div className="w-[510px] h-[610px] bg-[#ebfccb] rounded-lg m-5 mt-[-630px] ml-[750px] flex flex-col justify-center items-center text-center p-5">
         <div className="text-center p-5 mb-7">
           <span className="uppercase font-bold text-lg">DEADLIFTS</span>
         </div>
@@ -74,6 +74,33 @@ const MoreInfoPage = () => {
         </div>
       </div>
 
+      <h2 style={{ position: 'absolute', top: '2240px', left: '100px', color: 'black', padding: '5px', fontSize: "40px", fontWeight: 'semi-bold', textShadow: '2px 2px 4px rgba(0,0,0,0.5)' }}>
+        Nutrition Plan
+      </h2> 
+
+      <div className="mt-24 pb-20">
+        {/* New Smaller Boxes */}
+        <div className="flex justify-between gap-x-4 mt-40 mx-20">
+          <div className="w-[400px] h-[300px] bg-[#ebfccb] rounded-lg flex flex-col justify-center items-center text-center p-4">
+            <img src="/omlette.png" alt="Description of Image 1" className="w-4/5 h-auto -mb-1 -mt-8" />
+            <span className="uppercase font-bold text-sm -mt-5">Breakfast</span>
+            <div>Omelette with Spinach, Tomatoes, and Whole-Grain Toast</div>
+          </div>
+
+          <div className="w-[400px] h-[300px] bg-[#ebfccb] rounded-lg flex flex-col justify-center items-center text-center p-4">
+            <img src="/chicken.png" alt="Description of Image 1" className="w-3/5 h-auto -mb-1 -mt-2" />
+            <span className="uppercase font-bold text-sm">Lunch</span>
+            <div>Grilled Chicken Salad</div>
+          </div>
+
+          <div className="w-[400px] h-[300px] bg-[#ebfccb] rounded-lg flex flex-col justify-center items-center text-center p-4">
+            <img src="/salmon.png" alt="Description of Image 1" className="w-4/5 h-auto -mb-7 -mt-1" />
+            <span className="uppercase font-bold text-sm">Dinner</span>
+            <div>Salmon with Sweet Potatoes and Steamed Broccoli</div>
+          </div>
+        </div>
+      </div>
+
 
 
       
@@ -81,3 +108,4 @@ const MoreInfoPage = () => {
   );
 };
 export default MoreInfoPage;
+
