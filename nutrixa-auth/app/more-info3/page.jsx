64bc755461e1a8a9dbec66f073f26befd9838879
chrizztinna <116527657+chrizztinna@users.noreply.github.com>
2024-08-15 15:54:22 -0400

    Update more info3 page.jsx

--- nutrixa-auth/app/more-info3/page.jsx
+++ nutrixa-auth/app/more-info3/page.jsx
@@ -19,7 +19,7 @@ const MoreInfo3Page = () => {
       metabolism elevated.
       </h2> 
 
-      <div className="w-[510px] h-[610px] bg-[#ebfccb] rounded-lg m-5 mt-[250px] ml-[100px] flex flex-col justify-center items-center text-center p-5">
+      <div className="w-[510px] h-[610px] bg-[#ebfccb] rounded-lg m-5 mt-[250px] ml-[150px] flex flex-col justify-center items-center text-center p-5">
         <div className="text-center p-4 mb-6">
           <span className="uppercase font-bold text-lg">BURPEES</span>
         </div>
@@ -33,7 +33,7 @@ const MoreInfo3Page = () => {
         </div>
       </div>
 
-      <div className="w-[510px] h-[610px] bg-[#ebfccb] rounded-lg m-5 mt-[-627px] ml-[700px] flex flex-col justify-center items-center text-center p-5">
+      <div className="w-[510px] h-[610px] bg-[#ebfccb] rounded-lg m-5 mt-[-627px] ml-[750px] flex flex-col justify-center items-center text-center p-5">
         <div className="text-center p-5 mb-7">
           <span className="uppercase font-bold text-lg">KETTLEBALL SWINGS</span>
         </div>
@@ -48,7 +48,7 @@ const MoreInfo3Page = () => {
       </div>
 
 
-      <div className="w-[510px] h-[610px] bg-[#ebfccb] rounded-lg m-5 mt-[70px] ml-[100px] flex flex-col justify-center items-center text-center p-5">
+      <div className="w-[510px] h-[610px] bg-[#ebfccb] rounded-lg m-5 mt-[70px] ml-[150px] flex flex-col justify-center items-center text-center p-5">
         <div className="text-center p-5 mb-20 -mt-20">
           <span className="uppercase font-bold text-lg">ROWING</span>
         </div>
@@ -57,14 +57,14 @@ const MoreInfo3Page = () => {
        
         <div className="mb-[-90px] -mt-3">
           Target: Full body, especially legs, back, arms, and cardiovascular system<br />
-          Variations:Steady-State Rowing, Interval Rowing, Power Strokes
+          Variations: Steady-State Rowing, Interval Rowing, Power Strokes
         </div>
       </div>
 
 
 
 
-      <div className="w-[510px] h-[610px] bg-[#ebfccb] rounded-lg m-5 mt-[-630px] ml-[700px] flex flex-col justify-center items-center text-center p-5">
+      <div className="w-[510px] h-[610px] bg-[#ebfccb] rounded-lg m-5 mt-[-630px] ml-[750px] flex flex-col justify-center items-center text-center p-5">
         <div className="text-center p-5 mb-20 -mt-20">
           <span className="uppercase font-bold text-lg ">MOUNTAIN CLIMBERS</span>
         </div>
@@ -73,9 +73,37 @@ const MoreInfo3Page = () => {
        
         <div className="mb-[-90px] mt-10">
           Target: Hamstrings, glutes, lower back, core<br />
-          Variation: Conventional deadlifts, sumo deadlifts, Romanian deadlifts 
+          Variation: Cross-Body Mountain Climbers, Elevate Mountain Climbers, Spider Mountain Climbers 
         </div>
       </div>
+
+      <h2 style={{ position: 'absolute', top: '2275px', left: '100px', color: 'black', padding: '5px', fontSize: "40px", fontWeight: 'semi-bold', textShadow: '2px 2px 4px rgba(0,0,0,0.5)' }}>
+        Nutrition Plan
+      </h2> 
+
+      <div className="mt-24 pb-20">
+        {/* New Smaller Boxes */}
+        <div className="flex justify-between gap-x-4 mt-40 mx-20">
+          <div className="w-[400px] h-[300px] bg-[#ebfccb] rounded-lg flex flex-col justify-center items-center text-center p-4">
+            <img src="/chia.png" alt="Description of Image 1" className="w-3/5 h-auto mb-3 -ml-5 -mt-1" />
+            <span className="uppercase font-bold text-sm -mt-3">Breakfast</span>
+            <div>Chia Seed Pudding</div>
+          </div>
+
+          <div className="w-[400px] h-[300px] bg-[#ebfccb] rounded-lg flex flex-col justify-center items-center text-center p-4">
+            <img src="/asparagus.png" alt="Description of Image 1" className="w-3/5 h-auto -mb-1 -mt-2" />
+            <span className="uppercase font-bold text-sm">Lunch</span>
+            <div>Baked Salmon with Asparagus</div>
+          </div>
+
+          <div className="w-[400px] h-[300px] bg-[#ebfccb] rounded-lg flex flex-col justify-center items-center text-center p-4">
+            <img src="/stirfry.png" alt="Description of Image 1" className="w-4/5 h-auto -mb- -mt-1" />
+            <span className="uppercase font-bold text-sm">Dinner</span>
+            <div>Shrimp Stir-Fry with Cauliflower Rice</div>
+          </div>
+        </div>
+      </div>
+
 
 
 
@@ -84,3 +112,4 @@ const MoreInfo3Page = () => {
   );
 };
 export default MoreInfo3Page;
+
